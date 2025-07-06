3c93b709a3a1d6d28a2c8b7f90314c7c9114a7d7
Rounak Jaiswal <ROUNAKJAISWALOFFICIAL@GMAIL.COM>
2025-07-06 10:51:04 +0530

    updated employee details and backen api

---
+++ src/main/java/com/asset/asset_management/controller/EmployeeAssetController.java
@@ -0,0 +1,30 @@
+package com.asset.asset_management.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.asset.asset_management.model.AssetReport;
+import com.asset.asset_management.service.ReportService;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeAssetController {
+
+    @Autowired
+    private ReportService reportService;
+
+    /**
+     * Returns a list of assets assigned to the specified employee.
+     *
+     * Endpoint: GET /api/employees/{id}/assigned-assets
+     */
+    @GetMapping("/{id}/assigned-assets")
+    public List<AssetReport> getAssignedAssetsForEmployee(@PathVariable Long id) {
+        return reportService.getAssetsAssignedToEmployee(id);
+    }
+}
